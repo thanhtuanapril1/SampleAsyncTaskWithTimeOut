b7401c05c342197fc6246042286f697e138e78b6
thanhtuanapril1 <thanhtuan.april1@gmail.com>
2024-07-28 15:34:55 +0700

    update

--- SampleAsyncTaskWithTimeOut/Form1.cs
+++ SampleAsyncTaskWithTimeOut/Form1.cs
@@ -8,45 +8,84 @@ namespace WinFormsApp1
         {
             InitializeComponent();
         }
+
+        //Example 1
         private async void button1_Click(object sender, EventArgs e)
         {
             try
             {
                 textBox1.Text = $"Processing...";
+
                 int timeout = int.Parse(tbxTimeout.Text);
                 int num = int.Parse(tbxNumberInput.Text);
 
-                var cts = new CancellationTokenSource();
-                cts.CancelAfter(timeout);
+                Task timeoutTask = Task.Delay(timeout);     // Set desired connection timeout in milliseconds
+                Task<string> task = HeavyTask(num);
 
-                textBox1.Text = await HeavyTask(cts.Token);
+
+                if (await Task.WhenAny(task, timeoutTask) == task)
+                {
+                    textBox1.Text = task.Result;
+                }
+                else
+                {
+                    textBox1.Text = "Timeout.";
+                }
             }
             catch (FormatException ex)
             {
                 textBox1.Text = $"Error parsing input: {ex.Message}";
             }
         }
-        public async Task<string> HeavyTask(CancellationToken cancellationToken)
+        public async Task<string> HeavyTask(int num)
         {
-            int num = int.Parse(tbxNumberInput.Text);
+            await Task.Delay(num);
 
-            var result = await Task.Run(async () =>
-            {
-                try
-                {
-                    await Task.Delay(num,cancellationToken);    //This will throw an exception when cancellationToken active
-
-                    return $"Done: {num} loop";
-                }
-                catch (OperationCanceledException cancelException)
-                {
-                    return cancelException.Message;
-                }
-            }, cancellationToken);
-
-            return result;
+            return $"Done: {num} loop";
         }
 
+        ////Example 2
+        //private async void button1_Click(object sender, EventArgs e)
+        //{
+        //    try
+        //    {
+        //        textBox1.Text = $"Processing...";
+        //        int timeout = int.Parse(tbxTimeout.Text);
+        //        int num = int.Parse(tbxNumberInput.Text);
+
+        //        var cts = new CancellationTokenSource();
+        //        cts.CancelAfter(timeout);
+
+        //        textBox1.Text = await HeavyTask(cts.Token);
+        //    }
+        //    catch (FormatException ex)
+        //    {
+        //        textBox1.Text = $"Error parsing input: {ex.Message}";
+        //    }
+        //}
+        //public async Task<string> HeavyTask(CancellationToken cancellationToken)
+        //{
+        //    int num = int.Parse(tbxNumberInput.Text);
+
+        //    var result = await Task.Run(async () =>
+        //    {
+        //        try
+        //        {
+        //            await Task.Delay(num,cancellationToken);    //This will throw an exception when cancellationToken active
+
+        //            return $"Done: {num} loop";
+        //        }
+        //        catch (OperationCanceledException cancelException)
+        //        {
+        //            return cancelException.Message;
+        //        }
+        //    }, cancellationToken);
+
+        //    return result;
+        //}
+
+
+        ////Example 3
         //private async void button1_Click(object sender, EventArgs e)
         //{
         //    textBox1.Text = "Processing...";
